e527c113794ceea2599b11325bd6a34063114771
Yatashdeep <32187108+Yatashdeep@users.noreply.github.com>
2019-05-06 19:13:36 +0530

    hope

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
+    if(req.method ===   'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use('/',home);
 
 app.set('views', path.join(__dirname, 'views'));
