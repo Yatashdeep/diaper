28f2a345dae97bc829878b3d0f855c68507b258b
Yatashdeep <32187108+Yatashdeep@users.noreply.github.com>
2019-05-06 19:05:54 +0530

    c

--- app.js
+++ app.js
@@ -13,18 +13,7 @@ app.use('/',home);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use(morgan('dev'));
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-    );
-    if(req.method ===   'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+
 const dba='mongodb://yatash:vishal786@ds151076.mlab.com:51076/diaperchange'
 mongoose.connect(dba,{useNewUrlParser:true},function(error){
     if(error) console.log(error);
@@ -32,16 +21,4 @@ mongoose.connect(dba,{useNewUrlParser:true},function(error){
 })
 mongoose.set('useCreateIndex',true)
 var db=mongoose.connection
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-    );
-    if(req.method ===   'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
 module.exports=app;
