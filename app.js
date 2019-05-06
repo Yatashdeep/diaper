59726dafd6298d069875300a6787efea0a4a26e4
Yatashdeep <32187108+Yatashdeep@users.noreply.github.com>
2019-05-06 16:18:17 +0530

    changecomment

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.use('/',home);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use(morgan('dev'));
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
+    if(req.method ===   'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 const dba='mongodb://yatash:vishal786@ds151076.mlab.com:51076/diaperchange'
 mongoose.connect(dba,{useNewUrlParser:true},function(error){
     if(error) console.log(error);
