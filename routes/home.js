6ad0f5252d5f974aeec4d70cc0803af3cc63a6b6
Yatashdeep <32187108+Yatashdeep@users.noreply.github.com>
2019-11-07 14:28:25 +0530

    change routes

--- routes/home.js
+++ routes/home.js
@@ -12,4 +12,5 @@ router.post('/login',userapi.user_login)
 router.post('/password_reset',userapi.password_reset)
 router.get('/reset_password',userapi.resetpassword)
 router.post('/password_match',userapi.password_match)
+router.get('/user_detail',userapi.user_detail)
 module.exports=router
